62495f32d0fb6bc740bff1bc43e6dcf234346e56
ludwici <gammaclone@gmail.com>
2026-07-11 04:41:26 +0300

    git init

---
+++ src/main/java/com/ludwici/cri/CobblemonRaidDenIcons.java
@@ -0,0 +1,29 @@
+package com.ludwici.cri;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(CobblemonRaidDenIcons.MODID)
+public class CobblemonRaidDenIcons {
+    public static final String MODID = "cri";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public CobblemonRaidDenIcons(IEventBus modEventBus, ModContainer modContainer) {
+        NeoForge.EVENT_BUS.register(this);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
---
+++ src/main/java/com/ludwici/cri/CobblemonRaidDenIconsClient.java
@@ -0,0 +1,22 @@
+package com.ludwici.cri;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = CobblemonRaidDenIcons.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = CobblemonRaidDenIcons.MODID, value = Dist.CLIENT)
+public class CobblemonRaidDenIconsClient {
+    public CobblemonRaidDenIconsClient(ModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+    }
+}
