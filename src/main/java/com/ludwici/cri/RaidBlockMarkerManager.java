e6525e7bcc100dc757b30f8bfe08850e35ca35d0
ludwici <gammaclone@gmail.com>
2026-07-18 14:33:41 +0300

    Added tera type compat

--- src/main/java/com/ludwici/cri/RaidBlockMarkerManager.java
+++ src/main/java/com/ludwici/cri/RaidBlockMarkerManager.java
@@ -5,6 +5,7 @@ import com.cobblemon.mod.common.api.types.ElementalTypes;
 import com.ludwici.cri.network.RaidBossHolder;
 import com.necro.raid.dens.common.blocks.entity.RaidCrystalBlockEntity;
 import com.necro.raid.dens.common.client.ClientRaidRegistry;
+import com.necro.raid.dens.common.data.raid.RaidType;
 import journeymap.api.v2.client.IClientAPI;
 import journeymap.api.v2.client.display.DisplayType;
 import journeymap.api.v2.client.display.MarkerOverlay;
@@ -12,6 +13,7 @@ import journeymap.api.v2.client.model.MapImage;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+import net.neoforged.fml.ModList;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -55,11 +57,21 @@ public class RaidBlockMarkerManager {
 
         var raidType = holder.raidType();
 
-        int diameter = 36;
-        var element = ElementalTypes.get(raidType.name());
-        int offset = element.getTextureXMultiplier();
+        MapImage typeIcon;
 
-        MapImage typeIcon = new MapImage(ResourceLocation.fromNamespaceAndPath(Cobblemon.MODID, "textures/gui/types.png"), diameter * offset, diameter, diameter, diameter, 0xffffff, 1f);
+        if (raidType != RaidType.STELLAR) {
+            int diameter = 36;
+            var element = ElementalTypes.get(raidType.name());
+
+            int offset = element.getTextureXMultiplier();
+            typeIcon = new MapImage(ResourceLocation.fromNamespaceAndPath(Cobblemon.MODID, "textures/gui/types.png"), diameter * offset, diameter, diameter, diameter, 0xffffff, 1f);
+        } else if (ModList.get().isLoaded("mega_showdown")) {
+            typeIcon = new MapImage(ResourceLocation.parse(String.format("mega_showdown:textures/gui/summary/tera_types/%s.png", raidType.getSerializedName())), 36, 36);
+        } else {
+            LOGGER.error("Invalid element: {}", raidType.name());
+            return;
+        }
+
         typeIcon.centerAnchors();
 
         MarkerOverlay marker = new MarkerOverlay(MODID, pos, typeIcon);
